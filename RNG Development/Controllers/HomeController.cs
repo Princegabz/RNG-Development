148d1a6c18ef02eff9f3c5fb4e0483b56d0ad2da
Gabriel Kyereahene Mensah <gabrielkojo77@gmail.com>
2025-02-06 17:17:51 +0200

    Added a demo about us page

--- RNG Development/Controllers/HomeController.cs
+++ RNG Development/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace RNG_Development.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
