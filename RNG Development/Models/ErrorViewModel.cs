2a7ee437aef6df32ec070965437e6ac34604d466
gabri <gabri@DESKTOP-ABA34U5>
2025-02-06 15:26:12 +0200

    Add project files.

---
+++ RNG Development/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RNG_Development.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
